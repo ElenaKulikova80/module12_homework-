b6c489c07b122cb04fe6c5aaf78ff20e1ca8b770
ytokarevskaya <lan2214@yandex.ru>
2020-10-05 22:39:15 +0300

    Проверено

--- 12.4.4.js
+++ 12.4.4.js
@@ -1,32 +1,60 @@
 ﻿function ElectricDevice(name){
-  this.gruppa = 'Электроприбор',
-  this.name=name
-}
-ElectricDevice.prototype.getStatus = function(name,status){
-    console.log(`${name} ${status}`);
-}
-function Computer(name,ram,cpu){
-  this.name=name,
-  this.ram,
-  this.cpu
-}
-function Lamp(name,manufactura){
-  this.name=name,
-  this.manufactura=manufactura
+  this.gruppa = 'Электроприбор';
+  this.name = name;
+  this.status = "off";
 }
 
-Lamp.prototype=new ElectricDevice();
-Lamp.prototype.getPower = function(name,P){
-    console.log(`${name} имеет мощность, равную ${P} Вт.`)
-};
-Computer.prototype=new ElectricDevice();
-Computer.prototype.getRamCpu = function(name,ram,cpu){
-    console.log(`${name} имеет оперативную память ${ram} ГБ, процессор ${cpu}.`)
-};
-const IncandescentLamp=new Lamp();
-const Notebook=new Computer();
+ElectricDevice.prototype.getStatus = function(){
+    console.log(`Прибор ${this.name} ${this.status === "on"? "включен" : "выключен"}`);
+}
 
-IncandescentLamp.getStatus('Лампа накаливания','On');
-IncandescentLamp.getPower('Лампа накаливания',1000);
-Notebook.getStatus('Ноутбук','Off');
-Notebook.getRamCpu('Ноутбук',4,'Core')
+ElectricDevice.prototype.turnOn = function() {
+  this.status = "on";
+}
+
+ElectricDevice.prototype.turnOff = function() {
+  this.status = "off";
+}
+
+function Computer(name, ram, cpu){
+  this.name = name;
+  this.ram = ram;
+  this.cpu = cpu;
+}
+
+function Lamp(name, power){
+  this.name = name;
+  this.power = power;
+}
+
+Lamp.prototype = new ElectricDevice();
+Lamp.prototype.getPower = function(){
+    console.log(`${this.name} имеет мощность, равную ${this.power} Вт.`)
+};
+
+Computer.prototype = new ElectricDevice();
+Computer.prototype.getRamCpu = function(){
+    console.log(`${this.name} имеет оперативную память ${this.ram} ГБ, процессор ${this.cpu}.`)
+};
+
+const incandescentLamp = new Lamp('Лампа накаливания', 1000);
+const notebook = new Computer('Ноутбук', 4, 'Core');
+
+incandescentLamp.turnOn();
+incandescentLamp.getStatus();
+incandescentLamp.turnOff();
+incandescentLamp.getStatus();
+incandescentLamp.getPower();
+notebook.getStatus();
+notebook.getRamCpu()
+
+// Задание выполнено не верно, есть несколько ошибок:
+// 1. Функция getStatus в таком виде, как сейчас, не имеет смысла, т.к. мы хотим получить сведения об объекте, но при этом выводим данные, которые сами же и передали в функцию (аргументы name и status). Чтобы метод работал корректно, надо использовать this - ссылку на текущий объект.
+// 2. Чтобы метод getStatus работал верно, в конструкторе ElectricDevice должно быть определено свойство, отвечающее за состояние прибора (вкл\выкл), например, свойство status
+// 3. В конструкторе Computer объекту не присваиваются свойства ram и cpu
+// 4. С методом getPower у Lamp такая же проблема, как и в пункте 1. Эта информация (имя и мощность) должна храниться в объекте, а с помощью метода мы только получаем эти свойства
+// 5. То же самое касается и метода getRamCpu у Computer
+// 6. По заданию нужно было определить метод, который отвечает за включение/выключение приборов из розетки. У вас такого метода нет.\
+// 7. И небольшое замечание по стилю кода: с большой буквы пишутся только названия классов и функций конструкторов. Остальные переменные должны быть названы в стиле camelCase. Это не приводит к ошиьке на уровне кода, но это общепринятый стандарт, и лучше ему следовать, чтобы в коде не было путаницы.
+
+// Постаралась выше исправить все перечисленные ошибки
